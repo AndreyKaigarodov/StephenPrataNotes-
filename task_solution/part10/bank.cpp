cd877496587ab64c48279151a96f54048117a81d
Kaigarodov Andrey <kaigarodov2012@yandex.ru>
2021-07-02 18:23:46 +0500

    question done 10/10

--- task_solution/part10/bank.cpp
+++ task_solution/part10/bank.cpp
@@ -2,31 +2,32 @@
 #include <iostream>
 #include <string.h>
 
-Bank::Bank(){
-    //m_name = new char[10];
+Bank::Bank()
+{
+    //m_name = new char;
     //m_account_num = new char;
-    //m_balance = 0;
+    m_balance = 0;
 }
 
-Bank::Bank(const char* name, const char account_num[17], long balance){
-    m_name = new char[strlen(name)+1];
+Bank::Bank(const char *name, const char account_num[17], long balance)
+{
+    m_name = new char[strlen(name) + 1];
     strcpy(m_name, name);
-    strncpy(m_account_num, account_num,15);
+    strcpy(m_account_num, account_num);
     m_balance = balance;
 }
 
-void Bank::getInfo(){
+void Bank::getInfo()
+{
     std::cout << m_account_num << std::endl;
     std::cout << m_name << std::endl;
     std::cout << m_balance << std::endl;
 }
 
-void Bank::deposit(long cash){
-
+void Bank::deposit(long cash)
+{
 }
 
-void Bank::withdraw(long cash){
-
+void Bank::withdraw(long cash)
+{
 }
-
-
